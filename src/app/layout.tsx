9eabb348a84037438f9fafbf7ef706895356501b
Maximilian Bronkhorst <maximilian.bronkhorst@protonmail.com>
2025-02-21 13:01:03 +0100

    tailwind + nextjs working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import Navigation from "./components/navbar/";
 import { Inter } from "next/font/google";
-
+import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
