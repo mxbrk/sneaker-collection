ac69b23bfef281751ed2f4c92d97f7432eb6f110
max <maximilian.bronkhorst@protonmail.com>
2025-02-21 14:25:21 +0100

    modified navbar

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -8,6 +8,12 @@ const Navbar = () => {
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
             <ul className="hidden md:flex gap-x-6 text-white">
+            <li>
+                <Link href="/">
+                  <p>Home</p>
+                </Link>
+              </li>
+
               <li>
                 <Link href="/settings">
                   <p>Settings</p>
