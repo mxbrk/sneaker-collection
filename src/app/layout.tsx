fedfcaf5492d69bdfcc0bee3a8837bb891685dcc
Maximilian Bronkhorst <maximilian.bronkhorst@protonmail.com>
2025-04-23 00:06:06 +0200

    added feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import CookieBanner from '@/components/CookieBanner';
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -42,6 +43,7 @@ export default function RootLayout({
       >
         <AuthProvider>
           {children}
+          <Analytics />
           <CookieBanner />
         </AuthProvider>
       </body>
