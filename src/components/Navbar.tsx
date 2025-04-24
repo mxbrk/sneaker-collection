ea7832daf79eb31c4d87f0a495336eb2f10a781e
Maximilian Bronkhorst <maximilian.bronkhorst@protonmail.com>
2025-04-24 11:43:13 +0200

    fixed z-index bug where elements flow over navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ export default function Navbar() {
   };
 
   return (
-    <header className="bg-white border-b border-[#e5e5e5] sticky top-0 z-10">
+    <header className="bg-white border-b border-[#e5e5e5] sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
